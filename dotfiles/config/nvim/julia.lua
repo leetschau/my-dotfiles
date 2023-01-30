c44ebd1d7ff36c732c6a96d2def8dfcc96228c64
Li Chao <clouds@isrc.iscas.ac.cn>
2023-01-30 17:48:27 +0800

    add vim plugin for Julia support

--- dotfiles/config/nvim/julia.lua
+++ dotfiles/config/nvim/julia.lua
@@ -1,4 +1,7 @@
 require "langbase"
 
+-- pkg: JuliaEditorSupport/julia-vim
+
 vim.keymap.set('n', '<space>a', 'yy<C-w><C-L>Go<esc>p7x<C-w><C-H>')  -- here 7x delete the leading `julia> `
 vim.keymap.set('v', '<space>a', 'y<C-w><C-L>Go<esc>p=G<C-w><C-H>')
+
