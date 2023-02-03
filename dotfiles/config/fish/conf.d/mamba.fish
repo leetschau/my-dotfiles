e23df2e6b00a1fe20995b6888a3d4797915f0e4a
Li Chao <clouds@isrc.iscas.ac.cn>
2023-02-03 11:19:45 +0800

    add mamba configuration

---
+++ dotfiles/config/fish/conf.d/mamba.fish
@@ -0,0 +1,8 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/leo/.local/opt/mambaRoot/bin/conda "shell.fish" "hook" $argv | source
+
+if test -f "/home/leo/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish"
+    source "/home/leo/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish"
+end
+# <<< conda initialize <<<
