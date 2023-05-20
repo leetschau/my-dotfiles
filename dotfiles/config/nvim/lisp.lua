cd1a24f921f5cd4ee29a114be957504da39fe201
Li Chao <clouds@isrc.iscas.ac.cn>
2023-05-21 00:06:27 +0800

    update lisp configurations

--- dotfiles/config/nvim/lisp.lua
+++ dotfiles/config/nvim/lisp.lua
@@ -2,6 +2,9 @@ require "langbase"
 
 vim.g.maplocalleader = ','
 
--- pkg: vim-scripts/paredit.vim
--- pkg: Olical/conjure
+vim.keymap.set('n', '<space>e', '<cmd>ConjureEvalCurrentForm<cr>')
+vim.keymap.set('n', '<space>t', '<cmd>ConjureEvalRootForm<cr>')
+vim.keymap.set('n', '<space>f', '<cmd>ConjureEvalFile<cr>')
 
+-- pkg: Olical/conjure
+-- pkg: gpanders/nvim-parinfer
