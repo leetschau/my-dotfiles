e65dd5977418ca26004d0a152f2bd4231bcf8fbd
Clouds Haliatus <leetschau@gmail.com>
2023-09-05 16:00:20 +0800

    update apps config

--- dotfiles/config/fish/conf.opt/mamba.fish
+++ dotfiles/config/fish/conf.opt/mamba.fish
@@ -1,8 +1,7 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/leo/.local/opt/mambaRoot/bin/conda "shell.fish" "hook" $argv | source
-
-if test -f "/home/leo/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish"
-    source "/home/leo/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish"
+if test -f ~/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish
+    eval ~/.local/opt/mambaRoot/bin/conda "shell.fish" "hook" $argv | source
 end
-# <<< conda initialize <<<
+
+if test -f ~/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish
+    source ~/.local/opt/mambaRoot/etc/fish/conf.d/mamba.fish
+end
--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-    source /usr/share/autojump/autojump.fish
     set -x PATH $HOME/.local/bin $PATH
     fish_vi_key_bindings
 end
